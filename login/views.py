d28d9f2d0f8f72f048b0953ce5fb2742211c42e4
alexanderDeb <jhon.carvajal01@unicatolica.edu.co>
2022-07-06 10:01:41 -0500

    solucion de permisos de admin

--- login/views.py
+++ login/views.py
@@ -1,11 +1,12 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import get_user_model
+from rest_framework import permissions
 
 from .serializers import userSerializer
 
 
 class userViewSet(viewsets.ModelViewSet):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = [permissions.AllowAny]
     serializer_class = userSerializer
     queryset = get_user_model().objects.all()
