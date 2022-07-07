b497fcb3b8f6a6b417adc17d8952b7bffe243cdc
alexanderDeb <jhon.carvajal01@unicatolica.edu.co>
2022-07-07 15:27:36 -0500

    se anexo un template al correo de recuperacion

--- login/views.py
+++ login/views.py
@@ -1,6 +1,8 @@
+from email.mime.multipart import MIMEMultipart
+from django.template.loader import render_to_string
 from email.mime.text import MIMEText
 from tkinter import E
-from rest_framework import viewsets,permissions
+from rest_framework import viewsets, permissions
 from rest_framework.permissions import IsAuthenticated
 from login.models import User
 import smtplib
@@ -14,23 +16,27 @@ class userViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
-def send_email(destinatario, subject, mensaje):
+
+def send_email(destinatario, subject):
     try:
-        mailServer = smtplib.SMTP(settings.EMAIL_HOST,settings.EMAIL_PORT)
+        mailServer = smtplib.SMTP(settings.EMAIL_HOST, settings.EMAIL_PORT)
         mailServer.ehlo()
         mailServer.starttls()
         mailServer.ehlo()
         mailServer.login(settings.EMAIL_HOST_USER,settings.EMAIL_HOST_PASSWORD)
 
         # Construimos el mensaje simple
-        mensaje = MIMEText(mensaje)
-        mensaje['From']=settings.EMAIL_HOST_USER
-        mensaje['To']=destinatario
-        mensaje['Subject']=subject
+        mensaje = MIMEMultipart()
+        mensaje['From'] = settings.EMAIL_HOST_USER
+        mensaje['To'] = destinatario
+        mensaje['Subject'] = subject
+
+        content = render_to_string('send_email.html')
+        mensaje.attach(MIMEText(content, 'html'))
 
         # Envio del mensaje
-        mailServer.sendmail(settings.EMAIL_HOST_USER,destinatario,mensaje.as_string())
+        mailServer.sendmail(settings.EMAIL_HOST_USER,destinatario, mensaje.as_string())
 
     except Exception as e:
         print(e)
-send_email('lucasgutmann0@gmail.com', 'Nuevo Correo de Alexander', """Hola, como estas""")
+send_email('alexcs252002@gmail.com','Correo de recuperacion de datos')
